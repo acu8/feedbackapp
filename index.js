74cc482bcb917bf9d71e0c2134690290b4df0da5
acu8 <ewatanabe786@gmail.com>
2023-08-05 20:56:36 +0900

    google ouath

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const key = require('./config/keys');
+require('./models/User');
 require('./services/passport');
+
+
+
+mongoose.connect(key.mongoURI);
+
 const app = express();
 
 require('./routes/authRoutes')(app);
